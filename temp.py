7c2dfd3004e94feff4fa8c4737dbd7487b738dcd
diwas dulal <diwasd7@gmail.com>
2020-12-28 19:45:56 -0600

    My First Commit

---
+++ temp.py
@@ -0,0 +1,6 @@
+birth_year = input("What is your birth year: ")
+death = 2070 - float(birth_year) 
+print("You have " + str(death) + " years until you are dead.")
+world = ("hello world")
+print(world.replace("hello", "Hello"))
+
