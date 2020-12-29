37f00ba30a78e3adab2140dfcabecbbe4134b084
diwas dulal <diwasd7@gmail.com>
2020-12-28 19:57:00 -0600

    My Edited Commit

--- temp.py
+++ temp.py
@@ -3,4 +3,5 @@ death = 2070 - float(birth_year)
 print("You have " + str(death) + " years until you are dead.")
 world = ("hello world")
 print(world.replace("hello", "Hello"))
+print("THIS IS NEW!!!")
 
